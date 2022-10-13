d763f06b8bd270a579d9fdf1a162be64956d1d07
Junaid Ali <Junaid Ali>
2022-10-13 16:38:47 +0500

    spell correct

--- database/migrations/2022_10_13_112319_create_blogs_table.php
+++ database/migrations/2022_10_13_112319_create_blogs_table.php
@@ -20,13 +20,12 @@ return new class extends Migration
             $table->tinyText('summary');
             $table->tinyInteger('published');
             $table->longText('content');
-            $table->string('title');
             $table->date('published_at');
             $table->unsignedBigInteger('blog_category_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
-            $table->foreign('blog_category_id')->references('id')->on('blog_cateogires');
+            $table->foreign('blog_category_id')->references('id')->on('blog_categories');
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
